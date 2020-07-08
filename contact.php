4c7b9fb9a856dc1fe57ed1fb48d389ca6eb3670d
Siddhant Kaura <siddhantkaura@gmail.com>
2020-07-08 19:04:21 +0530

    third commit

--- contact.php
+++ contact.php
@@ -1,12 +1,13 @@
 <?php
-$name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
-$contact = $_REQUEST['contactno'];
-$message = $_REQUEST['message'];
+$to = "siddhantkaura@gmail.com";
+$subject = "response from website";
+$message = "Hey I want to connect you";
+$headers = "From: abcdf";
 
-mail("siddhantkaura@gmail.com","web message",$message,"From: $name <$email> <$contact>");
-    
-echo"<script type='text/javascript'>alert('Your message sent succesfully');
-window.history.log(-1);
-</script>";    
+if(mail($to,$subject,$message,$headers)){
+    echo "Mail sent successfuly";
+}
+else{
+    echo "cannot send";
+}
 ?>
